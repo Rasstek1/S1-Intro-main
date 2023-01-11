40989bb6d2527ce3ec13c30f405d648bd58c2c19
RassTek78 <2295093@cegepoutaouais.qc.ca>
2023-01-11 15:57:26 -0500

    Modif et ajout de Javascript

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function  ajouter(){
+    var item = document.getElementById("desc").value;
+   document.getElementById("maListe").append(item);
+    alert("L'item est bien ajouté");
+}
+
+
+
+
+
+
 console.log("Bonjour");
