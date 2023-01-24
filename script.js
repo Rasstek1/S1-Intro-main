3fa190122c45b34b4ab8d524bae81182f1d09c34
RassTek78 <2295093@cegepoutaouais.qc.ca>
2023-01-24 10:17:10 -0500

    2023-01-24

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-compteur = 1;
+compteur = 4;//variable globale
 
 function ajouter() {
     let item = document.getElementById("desc").value;
@@ -18,22 +18,30 @@ function ajouter() {
 
 }
 
+function ajouterEmail(){
+    var email = document.getElementById("email").value;
+    document.forms[0].action ="mailto:"+email;
+}
+
+function effacer(){
+    document.forms[0].action = "";
+}
+
+console.log("Whats up Dude!")
 
 
 
 
-
-
-   /* nouveauInput = document.createElement("input");
-    nouveauInput.setAttribute("value", item);
+    nouveauInput = document.createElement("input");
     nouveauInput.setAttribute("hidden", true);
-    nouveauInput.setAttribute("name", "item", +compteur);
+    nouveauInput.setAttribute("name", "Item_"+compteur);
+    nouveauInput.setAttribute("value",item);
     compteur++;
     document.forms[0].append(nouveauInput);
-    //alert("L'item est bien ajouté");// Pop-up
+    alert("L'item est bien ajouté connard!!");// Pop-up
 
 
-}
+
 
 function ajouterEmail() {
     var email = document.getElementById("email").value;
@@ -42,4 +50,3 @@ function ajouterEmail() {
 
 console.log("Bonjour");
 
-*/
